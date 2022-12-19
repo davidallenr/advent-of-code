dbd2f9861bde2005f6ee5dad5928a3b0bed9092f
David Russell <davidallenr@gmail.com>
2022-12-19 10:06:05 -0500

    All day 1 complete

---
+++ 2022/day1/main.py
@@ -0,0 +1,36 @@
+# open the file
+with open('input.txt', 'r') as f:
+    # read the file
+    lines = f.readlines()
+
+# initialize the total and the list of highest totals
+total = 0
+highest_totals = []
+
+# loop through the lines
+for line in lines:
+    # if the line is blank
+    if line == '\r' or line == '\n':
+        # add the current total to the list of highest totals
+        highest_totals.append(total)
+        # sort the list in descending order
+        highest_totals.sort(reverse=True)
+        # keep only the top three highest totals
+        highest_totals = highest_totals[:3]
+        # reset the total
+        total = 0
+    # if the line is not blank
+    else:
+        # add the number to the total
+        total += int(line)
+
+# add the last total to the list of highest totals
+highest_totals.append(total)
+# sort the list in descending order
+highest_totals.sort(reverse=True)
+# keep only the top three highest totals
+highest_totals = highest_totals[:3]
+
+# add the top three highest totals and print the result
+result = sum(highest_totals)
+print(result)
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# advent-of-code
-repo for my advent of code challenges
+# Advent of Code
+
+This repository contains my solutions to the [Advent of Code](https://adventofcode.com/) challenges. Each year's challenges are stored in a separate directory, and the status of each day's challenge is indicated in the file name (e.g. `year\day.py`).
+
+## Usage
+
+To run a solution, navigate to the directory for the corresponding year and run the Python script for the desired day. For example:
+
+$ cd 2021
+$ python day1_completed.py
+
+Copy code
+
+## Disclaimer
+
+These solutions are provided for educational purposes only and may not be the most efficient or elegant solutions to the challenges. Use at your own risk.
